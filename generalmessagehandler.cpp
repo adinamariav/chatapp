7836d4fcee9abaf5537f9126ffa0e0c2a4656753
adinamariav <adinamariav22@gmail.com>
2020-12-07 17:19:29 -0800

    Am configurat logica interfetei la pagina chat

---
+++ generalmessagehandler.cpp
@@ -0,0 +1,6 @@
+#include "generalmessagehandler.h"
+
+GeneralMessageHandler::GeneralMessageHandler()
+{
+
+}
---
+++ generalmessagehandler.h
@@ -0,0 +1,11 @@
+#ifndef GENERALMESSAGEHANDLER_H
+#define GENERALMESSAGEHANDLER_H
+
+
+class GeneralMessageHandler
+{
+public:
+    GeneralMessageHandler();
+};
+
+#endif // GENERALMESSAGEHANDLER_H
